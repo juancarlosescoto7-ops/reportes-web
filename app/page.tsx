e7448b40a62e33bc1444521d65f49c5548e112bf
Juan Carlos Escoto Salvador <juancarlosescoto7@gmail.com>
2026-04-14 23:52:02 -0600

    test: quick sync changes II

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-10">
-      <h1 className="text-xl font-bold">Sistema OK</h1>
+      <h1 className="text-xl font-bold">Sistema pendiente</h1>
     </div>
   );
 }
