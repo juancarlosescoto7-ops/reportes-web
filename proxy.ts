9fb46391b75d642035f219dd06fa6fe582d300ab
Juan Carlos Escoto Salvador <juancarlosescoto7@gmail.com>
2026-06-14 22:48:47 -0600

    Elimina middleware y usa proxy de Next

--- proxy.ts
+++ proxy.ts
@@ -8,7 +8,7 @@ export async function proxy(request: NextRequest) {
 
   try {
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
     if (!supabaseUrl || !supabaseAnonKey) {
       console.error("Faltan variables de entorno de Supabase en Vercel.");
